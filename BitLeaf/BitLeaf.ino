c9a767deadc582d065c51598ec633194f4dc3aa1
BitTim <tim.anhalt.2004@protonmail.com>
2020-08-18 22:55:42 +0200

    Added Clock

--- BitLeaf/BitLeaf.ino
+++ BitLeaf/BitLeaf.ino
@@ -6,34 +6,32 @@
 // Constants
 // ================================
 
-#define BTN1PIN              7
-#define BTN2PIN              8
+#define BTN1PIN              8
+#define BTN2PIN              9
 
-#define LONGPRESSTIME      250
+#define LONGPRESSTIME      500
 
 #define SENSORPIN           A0
 
-#define LEDPIN               5
+#define LEDPIN              12
 #define NUMPIXPERPANEL       6
 #define NUMPANELS           10
 #define NUMPIXELS           NUMPANELS * NUMPIXPERPANEL
 
-const int panelIDs[] = {
-  { 0,  1,  2,  3, 58, 59},
-  { 4,  5, 54, 55, 56, 57},
-  { 6,  7,  8,  9, 52, 53},
-  {10, 11, 48, 49, 50, 51},
-  {12, 13, 14, 15, 46, 47},
-  {16, 17, 42, 43, 44, 45},
-  {18, 19, 20, 21, 40, 41},
-  {22, 23, 36, 37, 38, 39},
-  {24, 25, 26, 27, 34, 35},
-  {28, 29, 30, 31, 32, 33}
-};
+#define BRIGHTNESSSTEPS      8
 
-const int hourPanels[2] =   {6, 7};
-const int minutePanels[2] = {4, 5};
-const int secondPanels[2] = {2, 3};
+int panelIDs[NUMPANELS][NUMPIXPERPANEL] = {
+  { 1,  0, 59, 58,  2,  3},
+  {57, 56,  4,  5, 54, 55},
+  { 7,  6, 53, 52,  8,  9},
+  {51, 50, 10, 11, 12, 13},
+  {49, 48, 47, 46, 14, 15},
+  {45, 44, 16, 17, 18, 19},
+  {43, 42, 41, 40, 20, 21},
+  {39, 38, 22, 23, 36, 37},
+  {25, 24, 35, 34, 26, 27},
+  {33, 32, 28, 29, 30, 31}
+};
 
 enum modes
 {
@@ -53,8 +51,9 @@ struct Col
   int g = 0;
   int b = 0;
 
-  Col()        {this->r = 0; this->g = 0; this->b = 0;}
-  Col(r, g, b) {this->r = r; this->g = g; this->b = b;}
+  Col()                    {this->r = 0; this->g = 0; this->b = 0;}
+  Col(int a)               {this->r = a; this->g = a; this->b = a;}
+  Col(int r, int g, int b) {this->r = r; this->g = g; this->b = b;}
 };
 
 // ================================
@@ -62,35 +61,40 @@ struct Col
 // ================================
 
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 ThreeWire rtcWire(3, 2, 4); //DAT, CLK, RST
 RtcDS1302<ThreeWire> rtc(rtcWire);
 RtcDateTime dt;
 
-bool btn1State = false;
-bool btn2State = false;
+int mode = CLOCK;
 
-bool btn1LongState = false;
-bool btnLong2State = false;
+int  btnPins[2]            = {BTN1PIN, BTN2PIN};
+bool btnJustPressed[2]     = {false, false};
+bool btnJustLongPressed[2] = {false, false};
+int  btnTimer[2]           = {0, 0};
 
-int btn1Timer = 0;
-int btn2Timer = 0;
+int prevBtnMap = 0;
 
-int mode = LIGHT;
+Col clockHourCol  = Col(156, 255,  13);
+Col clockMinCol   = Col(255, 236,  25);
+Col clockSecCol   = Col(255, 186,  13);
+Col clockOffCol = Col(  0,   0,   0);
 
-int binHour = 0;
-int binMinute = 0;
-int binSecond = 0;
+Col clockAMCol  = Col( 51, 213, 255);
+Col clockPMCol  = Col(255,   0,  88);
+
+int lampBrightness = 3;
 
 // ================================
 // Settings
 // ================================
 
-int brightness = 100;
-bool twelveHour = false;
+int brightness = 3;
+bool twelveHour = true;
 
-Col clockCol = Col(255, 255, 255);
+int lampOnHour = 17;
+int lampOffHour = 21;
 
 // ================================
 // Main
@@ -99,27 +103,24 @@ Col clockCol = Col(255, 255, 255);
 void setup() {
   rtc.Begin();
   rtc.SetIsRunning(true);
-
+  rtc.SetDateTime(RtcDateTime(__DATE__, __TIME__));
+  
   pixels.begin();
+  pixels.setBrightness(brightness);
+
+  pinMode(BTN1PIN, INPUT_PULLUP);
+  pinMode(BTN2PIN, INPUT_PULLUP);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  switch(mode)
-  {
-  case LIGHT:
-    modeLight();
-    break;
-  case CLOCK:
-    modeClock();
-    break;
-  case SETTINGS:
-    modeSettings();
-    break;
-  case EFFECTS:
-    modeEffects();
-    break;
-  }
+  int btnMap = getBtnMap();
   
+  if(btnMap == prevBtnMap) btnMap = 0;
+  else prevBtnMap = btnMap;
+  
+  runMode(btnMap);
   pixels.show();
 }
 
@@ -127,77 +128,117 @@ void loop() {
 // Modes
 // ================================
 
-void modeLight()
+void runMode(int btnMap)
 {
-  if(digitalRead(BTN1PIN) == HIGH)
+  switch(mode)
   {
-    if(btn1State == false)
-    {
-      btn1State = true;
-      btn1Timer = millis();
-    }
-    
-    if((millis() - btn1Timer > LONGPRESSTIME) && (btn1longState == false))
-    {
-      btn1LongState = true;
-      //Longpress
-    }
-  }
-  else
-  {
-    if(btn1State == true)
-    {
-      if(btn1LongState == true)
-      {
-        btn1LongState = false;
-      }
-      else
-      {
-        //Short Press
-      }
-      
-      btn1State = false;
-    }
+    case LIGHT:
+      modeLight(btnMap);
+      break;
+  
+    case CLOCK:
+      modeClock(btnMap);
+      break;
+  
+    case SETTINGS:
+      modeSettings(btnMap);
+      break;
+  
+    case EFFECTS:
+      modeEffects(btnMap);
+      break;
   }
 }
 
-void modeClock()
+void modeLight(int btnMap)
 {
+  if((btnMap >> 2) & 0x1) changeMode(CLOCK);
+  if((btnMap >> 0) & 0x1)
+  {
+    lampBrightness += 1;
+    if(lampBrightness >= BRIGHTNESSSTEPS) lampBrightness = 0;
+  }
+  
+  Col white = Col((256 / BRIGHTNESSSTEPS) * lampBrightness - 1);
+  fillLamp(white.r, white.g, white.b);
+}
+
+void modeClock(int btnMap)
+{
+  if((btnMap >> 2) & 0x1) changeMode(LIGHT);
+  
   dt = rtc.GetDateTime();
+
+  if(twelveHour)
+  {
+    int hour = dt.Hour();
+    bool pm = false;
+    
+    if(hour > 11) pm = true;
+    else pm = false;
+    
+    if(hour > 12) hour -= 12;
+    if(hour == 0) hour  = 12;
+    
+    binaryDisplay(hour, 6, 2, pm ? clockPMCol : clockAMCol, clockOffCol);
+  }
+  else binaryDisplay(dt.Hour(), 6, 2, clockHourCol, clockOffCol);
   
-  binaryDisplay(dt.Hour()  , hourPanels  , 2, 3);
-  binaryDisplay(dt.Minute(), MinutePanels, 2, 3);
-  binaryDisplay(dt.Second(), SecondPanels, 2, 3);
-  
-  
+  binaryDisplay(dt.Minute(), 4, 2, clockMinCol, clockOffCol);
+  binaryDisplay(dt.Second(), 2, 2, clockSecCol, clockOffCol);
+
+  if(lampOffHour < lampOnHour) lampOffHour += 24;
+  if(dt.Hour() > lampOnHour && dt.Hour() < lampOffHour)
+  {
+    Col white = Col(255);
+
+    setPanel(0, white.r, white.g, white.b);
+    setPanel(1, white.r, white.g, white.b);
+    setPanel(8, white.r, white.g, white.b);
+    setPanel(9, white.r, white.g, white.b);
+  }
+
+  if(lampOffHour > 23) lampOffHour -= 24;
+  if(dt.Hour() >= lampOffHour)
+  {
+    Col white = Col(0);
+
+    setPanel(0, white.r, white.g, white.b);
+    setPanel(1, white.r, white.g, white.b);
+    setPanel(8, white.r, white.g, white.b);
+    setPanel(9, white.r, white.g, white.b);
+  }
 }
 
-void modeSettings()
+void modeSettings(int btnMap)
 {
   
 }
 
-void modeEffects()
+void modeEffects(int btnMap)
 {
-  
+
 }
 
 // ================================
 // Util
 // ================================
 
-void binaryDisplay(int num, int *panels, int numPanels, int res)
+void binaryDisplay(int num, int panelOffset, int numPanels, Col on, Col off)
 {
+  Col col;
+  int resolution = (numPanels * NUMPIXPERPANEL) / 6;
+  
   for(int panel = 0; panel < numPanels; panel++)
   {
-    for(int pixel = 0; pixel < res; pixel++)
+    for(int digit = 0; digit < 6 / resolution; digit++)
     {
-      if(num & 0x1 == 1) Col col = clockCol;
-      else Col col = Col();
+      if(num & 0x1 == 1) col = on;
+      else col = off;
       
-      for(int i = 0; i < NUMPIXPERPANEL / res; i++)
+      for(int i = 0; i < resolution; i++)
       {
-        setPixel(panelIDs[panels[panel]][pixel + i], col.r, col.g, col.b);
+        setPixel(panelIDs[panel + panelOffset][digit * resolution + i], col.r, col.g, col.b);
       }
 
       num = num >> 1;
@@ -205,12 +246,52 @@ void binaryDisplay(int num, int *panels, int numPanels, int res)
   }
 }
 
-void setPixel(id, r, g, b)
+int getBtnMap()
+{
+  int btnMap = 0b0000;
+
+  for(int i = 0; i < 2; i++)
+  {
+    btnMap = btnMap << 2;
+    
+    if(digitalRead(btnPins[i]) == LOW)
+    {
+      if(!btnJustPressed[i]) {btnJustPressed[i] = true; btnTimer[i] = millis();}
+      if((millis() - btnTimer[i] > LONGPRESSTIME) && (btnJustLongPressed[i] == false)) btnMap |= 0b10;
+    }
+    else
+    {
+      if(btnJustPressed[i])
+      {
+        if(btnJustLongPressed[i]) btnJustLongPressed[i] = false;
+        else btnMap |= 0b01;
+
+        btnJustPressed[i] = false;
+      }
+    }
+  }
+
+  return btnMap;
+}
+
+// ================================
+// Helper
+// ================================
+
+void changeMode(int newMode)
+{
+  clearLamp();
+  mode = newMode;
+
+  pixels.show();
+}
+
+void setPixel(int id, int r, int g, int b)
 {
   pixels.setPixelColor(id, pixels.Color(r, g, b));
 }
 
-void setPanel(id, r, g, b)
+void setPanel(int id, int r, int g, int b)
 {
   for(int i = 0; i < NUMPIXPERPANEL; i++)
   {
@@ -223,7 +304,7 @@ void clearLamp()
   fillLamp(0, 0, 0);
 }
 
-void fillLamp(r, g, b)
+void fillLamp(int r, int g, int b)
 {
   for(int i = 0; i < NUMPIXELS; i++)
   {
